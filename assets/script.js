db3b5120f97aa8343bce6d6d87d299acc1531f17
srad25 <suzanne.radlein@gmail.com>
2021-02-21 13:51:11 -0500

    completed work on script

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,56 @@
-// display current day  and time on page
-  var todaysDate = moment().format('MMMM Do YYYY, h:mm:ss a'); 
-  $("#currentDay").html(todaysDate);
+$(document).ready(function () {
+    //display current day and time on page
+    var todaysDate = moment().format('MMMM Do YYYY, h:mm:ss a'); 
+    $("#currentDay").html(todaysDate);
 
 
-    //  Trigger click listener on saveBtn
+    //Set saveBtn click listener
     $(".saveBtn").on("click", function () {
-    //use the nearby values
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
+        //get nearby values.
+        var text = $(this).siblings(".description").val(); // taken the change from the sibling html description attribute
+        var time = $(this).parent().attr("id"); // taken the change from the parent html id attribute
 
-    // then save to local storage
+        //then save in local storage.
         localStorage.setItem(time, text);
-  });
+    })
+    //Get item from local storage if any for each hour
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+    
+
+    function timeKeeping() {
+        //get current number of hours.
+        var rightNow = moment().hour(); 
+
+        // loop over time blocks
+        $(".time-block").each(function () {
+            var clockHour = parseInt($(this).attr("id").split("hour")[1]);
+
+            //// Color coding time blocks according to the time (past, present, future).
+            if (clockHour < rightNow) {
+                $(this).addClass("past");
+               
+            }
+            else if (clockHour === rightNow) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+               
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+    }
+    timeKeeping(); 
+})
+
+
