5ea8f53afa4a4802f8f1c902c425c889d5ff4adc
srad25 <suzanne.radlein@gmail.com>
2021-02-20 23:16:13 -0500

    code for saveBtn

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,14 @@
-  // display current day on page
+// display current day  and time on page
   var todaysDate = moment().format('MMMM Do YYYY, h:mm:ss a'); 
   $("#currentDay").html(todaysDate);
 
+
+    //  Trigger click listener on saveBtn
+    $(".saveBtn").on("click", function () {
+    //use the nearby values
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+    // then save to local storage
+        localStorage.setItem(time, text);
+  });
